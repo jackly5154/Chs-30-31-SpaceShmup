d9acaf57f5926a27c443f152257a8ccf6298275f
jackly5154 <49957081+jackly5154@users.noreply.github.com>
2019-10-31 22:33:56 -0400

    completed the scripts and parts required.

--- Assets/__Scripts/Enemy_1.cs
+++ Assets/__Scripts/Enemy_1.cs
@@ -31,6 +31,7 @@ public class Enemy_1 : Enemy {
         Vector3 rot = new Vector3(0, sin * waveRotY, 0);
         this.transform.rotation = Quaternion.Euler(rot);
         base.Move();
+    //    print(bndCheck.isOnScreen);
     }
 
     // Override the Move function on Enemy
